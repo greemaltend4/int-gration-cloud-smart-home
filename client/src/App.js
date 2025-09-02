61cc72f4547e97e141b6a20a7982aa0b232c1b6c
greemaltend4 <dengestabiy@hotmail.com>
2025-09-02 11:25:57 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Intégration Cloud Smart Home</h1>
+    </div>
+  );
+}
+
+export default App;
